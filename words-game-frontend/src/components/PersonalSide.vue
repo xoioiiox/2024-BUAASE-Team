a979ebacf775467ff5e2e596d6bac4a036afdae2
P <864641067@qq.com>
2024-04-17 19:40:11 +0800

    [feat]: 完成个人中心-选择词书界面设置

---
+++ words-game-frontend/src/components/PersonalSide.vue
@@ -0,0 +1,72 @@
+<template>
+  <el-row>
+		<el-col>
+      <h5 class="mb-2">Default colors</h5>
+      <el-menu
+        default-active="1"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <div class="avatar">
+          <el-avatar :size="130" :src="circleUrl"></el-avatar>
+        </div>
+        <el-menu-item index="1" @click="toChooseBook()">
+          <el-icon><icon-menu /></el-icon>
+          <span>当前词书</span>
+        </el-menu-item>
+        <el-menu-item index="2" @click="toStatistic()">
+          <el-icon><document /></el-icon>
+          <span>统计信息</span>
+        </el-menu-item>
+        <el-menu-item index="3" @click="">
+          <el-icon><setting /></el-icon>
+          <span>生词本记录</span>
+        </el-menu-item>
+        <el-menu-item index="4" @click="">
+          <el-icon><setting /></el-icon>
+          <span>修改个人信息</span>
+        </el-menu-item>
+        <el-menu-item index="5" @click="">
+          <el-icon><setting /></el-icon>
+          <span>排行榜</span>
+        </el-menu-item>
+        <el-menu-item index="6" @click="toSetting()">
+          <el-icon><setting /></el-icon>
+          <span>学习设置</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+	export default {
+		name: "PersonalSide",
+		data() {
+			return {
+
+			};
+		},
+		methods: {
+			toChooseBook() {
+        this.$router.push({path:'/PersonalBook/'})
+      },
+			toStatistic() {
+        this.$router.push({path:'/PersonalStatistic/'})
+      },
+			toSetting() {
+        this.$router.push({path:'/StudySetting/'})
+      },
+		}
+	}
+</script>
+
+<style scoped>
+.avatar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ words-game-frontend/src/views/PersonalFunc/PersonalSetting.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <el-row>
+            <el-col :span="6">
+                <PersonalSide></PersonalSide>
+            </el-col>
+            <el-col :span="18">
+                <div class="ChooseBook">
+                    <el-card @click="ChooseThisBook()">
+                        四级单词
+                    </el-card>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+import PersonalSide from "../../components/PersonalSide.vue"
+export default {
+    components: {PersonalSide},
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+.ChooseBook {
+    margin-top: 100px;
+    margin-left: 30px;
+}
+.el-card {
+    width: 180px;
+    height: 220px;
+}
+</style>
