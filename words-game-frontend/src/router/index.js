bad64ef2f34ea6bdc4ff2715871a712a6640c129
wqq <65055058+wqqkakuiii@users.noreply.github.com>
2024-04-19 12:56:54 +0800

    [feat]:完成页面框架

--- words-game-frontend/src/router/index.js
+++ words-game-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PersonalBook from '../views/PersonalFunc/PersonalBook.vue'
-import PersonalSetting from "@/views/PersonalFunc/PersonalSetting.vue";
+import PersonalSetting from "@/views/PersonalFunc/PersonalSetting.vue"
+import WordDetail from '@/components/WordDetail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: 'PersonalSetting',
       component: PersonalSetting
     },
+    {
+      path: '/WordDetail',
+      name: 'WordDetail',
+      component: WordDetail
+    }
   ]
 })
 
