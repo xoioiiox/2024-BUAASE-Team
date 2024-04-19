7f54baeb58f4f402634e2b221a2b679841350577
nrll <naraa_togy@yahoo.com>
2024-04-19 19:42:44 +0800

    [feat]: added SavedWords page

--- words-game-frontend/src/router/index.js
+++ words-game-frontend/src/router/index.js
@@ -1,32 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import PersonalBook from '../views/PersonalFunc/PersonalBook.vue'
-import PersonalSetting from "@/views/PersonalFunc/PersonalSetting.vue"
-import WordDetail from '@/components/WordDetail.vue'
-import PersonalInfo from '../views/PersonalFunc/PersonalInfo.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import PersonalBook from "../views/PersonalFunc/PersonalBook.vue";
+import PersonalSetting from "@/views/PersonalFunc/PersonalSetting.vue";
+import WordDetail from "@/components/WordDetail.vue";
+import PersonalInfo from "../views/PersonalFunc/PersonalInfo.vue";
+import SavedWords from "../views/PersonalFunc/SavedWords.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/PersonalBook',
-      name: 'PersonalBook',
-      component: PersonalBook
+      path: "/PersonalBook",
+      name: "PersonalBook",
+      component: PersonalBook,
     },
     {
-      path: '/PersonalSetting',
-      name: 'PersonalSetting',
-      component: PersonalSetting
+      path: "/PersonalSetting",
+      name: "PersonalSetting",
+      component: PersonalSetting,
     },
     {
-      path: '/WordDetail',
-      name: 'WordDetail',
-      component: WordDetail
+      path: "/SavedWords",
+      name: "SavedWords",
+      component: SavedWords,
     },
-    {path: '/PersonalInfo',
-      name: 'PersonalInfo',
-      component: PersonalInfo
-    }
-  ]
-})
+    {
+      path: "/WordDetail",
+      name: "WordDetail",
+      component: WordDetail,
+    },
+    { path: "/PersonalInfo", name: "PersonalInfo", component: PersonalInfo },
+  ],
+});
 
-export default router
+export default router;
