3a0009be3a2460f8933494d5878cde960ccde449
nrll <naraa_togy@yahoo.com>
2024-04-26 02:03:10 +0800

    [feat]: added saved word card draft functions

--- words-game-frontend/src/views/PersonalFunc/SavedWords.vue
+++ words-game-frontend/src/views/PersonalFunc/SavedWords.vue
@@ -9,23 +9,25 @@
         <!-- Words List -->
         <el-scrollbar class="saved-words-list" max-height="600px" height="100%">
           <!-- Word Card/ -->
-
           <div class="saved-words-card" v-for="word in 30" :key="word">
-            <div class="word-card-title">
-              <div>Word</div>
-              <span>au</span>
-            </div>
-            <div class="word-card-desc">
-              Lorem ipsum dolor sit amet consectetur
-            </div>
+            <div class="word-card-body">
+              <div class="word-card-title">
+                <div class="word-card-word">Word {{ word }}</div>
+                <span class="word-card-speak" @click="onListenWord(word)">
+                  <IconSpeaker />
+                </span>
+              </div>
 
-            <span class="word-card-saved">
-              <el-icon :size="10" color="#fc4085">
-                <StarFilled
-                  style="width: 1em; height: 1em; margin-right: 8px"
-                />
-              </el-icon>
-            </span>
+              <div class="word-card-desc" @click="onShowWord(word)">
+                Lorem ipsum dolor sit amet consectetur
+                <a> More >>></a>
+              </div>
+            </div>
+            <div class="word-card-saved">
+              <span class="word-card-saved-icon" @click="onUnsaveWord(word)">
+                <IconStar />
+              </span>
+            </div>
           </div>
         </el-scrollbar>
       </el-col>
@@ -34,11 +36,14 @@
 </template>
 
 <script>
+import IconStar from "@/components/icons/IconStar.vue";
+import IconSpeaker from "@/components/icons/IconSpeaker.vue";
+import { ElMessage, ElMessageBox } from "element-plus";
 import PersonalSide from "../../components/PersonalSide.vue";
 // import axios from "axios";
 
 export default {
-  components: { PersonalSide },
+  components: { PersonalSide, IconStar, IconSpeaker },
   // async created() {
   //   await axios({
   //     method: "get",
@@ -49,10 +54,43 @@ export default {
   //     },
   //   }).then((res) => console.log("res", res));
   // },
-  methods: {},
+  methods: {
+    onShowWord(word) {
+      ElMessage({
+        type: "info",
+        message: `Go to ${word} detail`,
+      });
+    },
+    onListenWord(word) {
+      ElMessage({
+        type: "info",
+        message: `Listen to ${word}`,
+      });
+    },
+    onUnsaveWord(word) {
+      ElMessageBox.confirm("", {
+        title: `Do you want to unsave ${word}?`,
+        confirmButtonText: "Yes",
+        cancelButtonText: "Cancel",
+        type: "warning",
+      })
+        .then(() => {
+          ElMessage({
+            type: "success",
+            message: "Delete completed",
+          });
+        })
+        .catch(() => {
+          ElMessage({
+            type: "info",
+            message: "Delete canceled",
+          });
+        });
+    },
+  },
   data() {
     return {
-      words: ["Hello", "Goodbye", "Dummy"],
+      words: [],
     };
   },
 };
@@ -78,33 +116,50 @@ export default {
 }
 
 .saved-words-card {
+  display: flex;
   width: 100%;
   height: 80px;
   margin-bottom: 10px;
   padding: 10px 12px;
   box-shadow: rgba(0, 0, 0, 0.101) 0px 2px 4px;
   border-radius: 10px;
-  position: relative;
   border: 1px solid #e0e0e0d4;
 }
 
+.word-card-body {
+  width: 100%;
+}
+
 .word-card-title {
+  width: fit-content;
   display: flex;
   gap: 8px;
   font-size: 24px;
+  padding: 5px 0 1px;
+  /* border: 1px solid red; */
+}
+
+.word-card-speak {
+  width: 40px;
+  height: 24px;
   /* border: 1px solid red; */
 }
 
 .word-card-saved {
-  border: 1px solid blue;
+  width: 30px;
+  height: 30px;
+  position: relative;
+  /* border: 1px solid green; */
+}
+
+.word-card-saved-icon {
   position: absolute;
-  top: 16px;
-  right: 16px;
 }
 
 .word-card-desc {
   color: grey;
   font-size: 14px;
+  /* border: 1px solid red; */
 }
 </style>
 
