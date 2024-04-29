a188dd622ef087a19f8033c98dfdba7933ca52c2
Ren Fly <1753856870@qq.com>
2024-04-29 13:18:25 +0000

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ import { useUserStore } from '@/stores/userStore.js';
 import PunchIn from '../components/PunchIn.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
