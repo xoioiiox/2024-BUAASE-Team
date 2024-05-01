508814add48ebbcbc96769ba0ebeac425972dada
nrll <naraa_togy@yahoo.com>
2024-05-01 15:53:13 +0800

    fixed personal stats data, UI

--- words-game-frontend/src/views/PersonalFunc/PersonalStats.vue
+++ words-game-frontend/src/views/PersonalFunc/PersonalStats.vue
@@ -1,19 +1,24 @@
+//Stats
 <template>
   <div>
     <el-row :gutter="20">
+      <!-- PERSONAL SIDE -->
       <el-col :span="6">
         <PersonalSide />
       </el-col>
-      <el-col :span="16" class="stat-page">
+      <!-- STAT PAGE -->
+      <el-col :span="18" class="stat-page">
         <h2>统计信息</h2>
-        <!-- Row 1 -->
-        <el-row :span="20" :gutter="20">
-          <el-col :span="8">
-            <div class="stat-windows stat-daily">
-              <el-row :gutter="16">
-                <el-col :span="24">
-                  <!-- Card -->
-                  <div class="stat-card">
+
+        <el-row :span="24" :gutter="20">
+          <el-col :span="12">
+            <!-- STUDY STATS -->
+            <div class="stat-study">
+              <!-- DAILY -->
+              <el-row class="stat-daily-row stat-windows">
+                <!-- Card -->
+                <el-col :span="6" class="stat-card">
+                  <div>
                     <el-statistic title="新学" :value="dataToday.learn_number">
                       <template #suffix>
                         <el-icon style="font-size: small"> 词 </el-icon>
@@ -21,10 +26,11 @@
                     </el-statistic>
                   </div>
                 </el-col>
+                <el-divider direction="vertical" />
 
                 <!-- Card 2 -->
-                <el-col :span="24">
-                  <div class="stat-card">
+                <el-col :span="6" class="stat-card">
+                  <div>
                     <el-statistic title="复习" :value="dataToday.review_number">
                       <template #suffix>
                         <el-icon style="font-size: small"> 词 </el-icon>
@@ -32,29 +38,33 @@
                     </el-statistic>
                   </div>
                 </el-col>
+                <el-divider direction="vertical" />
 
                 <!-- Card 3 -->
-                <el-col :span="8">
-                  <div class="stat-card">
+                <el-col :span="8" class="stat-card">
+                  <div>
                     <el-statistic title="复习" :value="dataToday.time">
                     </el-statistic>
                   </div>
                 </el-col>
               </el-row>
+
+              <!-- WEEKLY -->
+              <el-row class="stat-weekly-row">
+                <el-col :span="24">
+                  <div class="stat-windows stat-weekly-graph">
+                    近7天
+                    <Line :options="options" :data="data" />
+                  </div>
+                </el-col>
+              </el-row>
             </div>
           </el-col>
-          <el-col :span="12">
+
+          <!-- CALENDAR -->
+          <el-col :span="8">
             <div class="stat-windows stat-calendar">
               <el-calendar v-model="value" />
-            </div>
-          </el-col>
-        </el-row>
-        <!-- Row 2 -->
-        <el-row class="stat-weekly-row">
-          <el-col :span="20">
-            <div class="stat-windows stat-weekly">
-              近7天
-              <Line :options="options" :data="data" />
             </div>
           </el-col>
         </el-row>
@@ -67,7 +77,6 @@
 import axios from "axios";
 import PersonalSide from "../../components/PersonalSide.vue";
 import { onBeforeMount, ref } from "vue";
-
 //CHART
 import {
   Chart as ChartJS,
@@ -91,14 +100,65 @@ ChartJS.register(
   Legend
 );
 
+//API FILLED DATA
+//if failed to fetch show error
+const dataToday = {
+  learn_number: 3,
+  review_number: 5,
+  time: "0:00:10",
+};
+
+//今天，昨天，前天，大前天...
+const dataWeek = [
+  {
+    learn_num: 3,
+    review_num: 5,
+    time: "0:10:34",
+  },
+  {
+    learn_num: 5,
+    review_num: 10,
+    time: "0:20:02",
+  },
+  {
+    learn_num: 12,
+    review_num: 4,
+    time: "0:21:00",
+  },
+  {
+    learn_num: 10,
+    review_num: 12,
+    time: "1:30:02",
+  },
+  {
+    learn_num: 10,
+    review_num: 20,
+    time: "1:30:02",
+  },
+  {
+    learn_num: 7,
+    review_num: 12,
+    time: "1:30:02",
+  },
+  {
+    learn_num: 0,
+    review_num: 16,
+    time: "1:30:02",
+  },
+];
 
 const data = ref({
   labels: ["7", "6", "5", "4", "3", "2", "1"],
   datasets: [
     {
-      label: "Study progress",
+      label: "学习词数",
       backgroundColor: "#0096FF",
-      data: [100, 20, 50, 2, 60, 30, 10],
+      data: dataWeek.reverse().map((dataDay) => dataDay.learn_num),
+    },
+    {
+      label: "复习词数",
+      backgroundColor: " #00FF00",
+      data: dataWeek.reverse().map((dataDay) => dataDay.review_num),
     },
   ],
 });
@@ -109,19 +169,6 @@ const options = {
 
 //CALENDAR
 const value = ref(new Date());
-
-//API FILLED DATA
-const dataToday = {
-  time: "0:00:10",
-  learn_number: 3,
-  review_number: 2,
-};
-
-const dataDays = {
-  time: "0:00:10",
-  learn_number: 3,
-  review_number: 2,
-};
 
 // FUNCTIONS
 /*
@@ -150,36 +197,46 @@ onBeforeMount(() => {
 </script>
 
 <style scoped>
-.stat-windows {
-  border-radius: 4px;
-  padding: 0 8px;
-  box-shadow: rgba(17, 17, 26, 0.05) 0px 1px 0px,
-    rgba(17, 17, 26, 0.1) 0px 0px 8px;
-}
-
-.el-calendar {
-  --el-calendar-cell-width: 36px;
+* {
+  box-sizing: border-box;
 }
 
 .stat-page {
+  /* border: 1px solid red; */
   height: 100%!;
+  padding-left: 50px;
 }
 
-.stat-daily {
+.stat-windows {
+  /* border: 1px solid blue; */
+  border-radius: 4px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px,
+    rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;
+}
+
+.stat-study {
+  /* border: 1px solid red; */
   height: 100%;
+  gap: 5px;
+}
+
+.stat-daily-row {
+  /* border: 1px solid orange; */
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .stat-weekly-row {
-  position: relative;
-  margin-top: 20px;
+  margin-top: 40px;
   height: 20%;
 }
 
-.stat-weekly {
+.stat-weekly-graph {
+  padding: 10px 20px 4px 10px;
   height: 100%;
 }
-
-/* Copy Pasted */
 
 .el-statistic {
   --el-statistic-content-font-size: 28px;
@@ -191,8 +248,17 @@ onBeforeMount(() => {
   border-radius: 4px;
   background-color: var(--el-bg-color-overlay);
 }
+
 .stat-card-title {
   display: inline-flex;
   align-items: center;
+}
+
+.el-calendar {
+  --el-calendar-cell-width: 36px;
+}
+
+.el-divider {
+  height: 50px;
 }
 </style>
