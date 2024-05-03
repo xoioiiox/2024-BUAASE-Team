3d03b7656800797dd4c02a2f284e714db804420a
nrll <naraa_togy@yahoo.com>
2024-05-03 13:00:24 +0800

    added axios req in saved words

--- words-game-frontend/vite.config.js
+++ words-game-frontend/vite.config.js
@@ -1,48 +1,46 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   server: {
+    host: "127.0.0.1",
 
-    host: '127.0.0.1',
-
-    port: '3000',    // 指定的本地启动端口
+    port: "3000", // 指定的本地启动端口
     proxy: {
-      '/api': {     // 你想代理的请求
-        target: 'http://127.0.0.1:8080/',        // target是你想请求的后端地址
-        changeOrigin: true,    // 允许跨域
-        rewrite: (path) => path.replace(/^\/api/, '') // 不可以省略rewrite
+      "/api": {
+        // 你想代理的请求
+        target: "http://60.205.14.77:8998/api", // target是你想请求的后端地址
+        changeOrigin: true, // 允许跨域
+        rewrite: (path) => path.replace(/^\/api/, ""), // 不可以省略rewrite
       },
-      '/baiduapi': {
-        target: 'https://aip.baidubce.com/',
+      "/baiduapi": {
+        target: "https://aip.baidubce.com/",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/baiduapi/, '')
+        rewrite: (path) => path.replace(/^\/baiduapi/, ""),
       },
-      '/fanyi': {
-        target: 'https://fanyi-api.baidu.com/',
+      "/fanyi": {
+        target: "https://fanyi-api.baidu.com/",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/fanyi/, '')
+        rewrite: (path) => path.replace(/^\/fanyi/, ""),
       },
-      '/smmsapi': {
-        target: 'https://smms.app/',
+      "/smmsapi": {
+        target: "https://smms.app/",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/smmsapi/, '')
-      }
-    }
-  },     // 实际请求中的写法：axois.post('/api/xxx', data) 【所以不需要配置axios的baseUrl了】
+        rewrite: (path) => path.replace(/^\/smmsapi/, ""),
+      },
+    },
+  }, // 实际请求中的写法：axois.post('/api/xxx', data) 【所以不需要配置axios的baseUrl了】
 
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
