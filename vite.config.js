0a212f88befc32abe846cc375a9b9c66a1838647
任宇翔 <21371283@buaa.edu.cn>
2024-05-08 22:14:01 +0800

    打包不会显示console,log

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild:{
+    pure: ['console.log'], // 删除 console.log
+    drop: ['debugger'], // 删除 debugger
+  },
+  
   plugins: [
     vue(),
   ],
